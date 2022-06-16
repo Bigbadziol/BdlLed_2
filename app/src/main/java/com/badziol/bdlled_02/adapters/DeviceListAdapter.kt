ade55cf6b7dabd7f4a9463ef67b28f7495edb481
Krzysztof Dłuski <link.dluski@op.pl>
2022-06-16 21:26:55 +0200

    poprawka adapter

--- app/src/main/java/com/badziol/bdlled_02/adapters/DeviceListAdapter.kt
+++ app/src/main/java/com/badziol/bdlled_02/adapters/DeviceListAdapter.kt
@@ -3,6 +3,7 @@ package com.example.bdlled_02
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.content.res.Resources
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import android.widget.ArrayAdapter
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+
 import com.badziol.bdlled_02.TAG
 
 enum class DEVICE_ACTION_TYPE{ADD,REMOVE,NOACTION}
@@ -50,23 +52,26 @@ class DeviceListAdapter(private val  context: Activity,
         }
 
         btn.setOnClickListener {
-            val device = bluetoothAdapter.getRemoteDevice(items[position].deviceAdress)
-            if (device != null) {
-                when (action) {
-                    DEVICE_ACTION_TYPE.ADD -> {
-                        Log.d(TAG, "click to add : -> ${device.name} : ${device.address} ")
-                        device.createBond()
-                    }
-                    DEVICE_ACTION_TYPE.REMOVE -> {
-                        Log.d(TAG, "click to remove : -> ${device.name} : ${device.address} ")
-                        try {
-                            device::class.java.getMethod("removeBond").invoke(device)
-                        } catch (e: Exception) {
-                            Log.e(TAG, "Removing bond has been failed. ${e.message}")
+            val f = context.getString(R.string.NO_DEVICE_NAME)
+            if (items[position].deviceName.contentEquals(f) == false) {
+                val device = bluetoothAdapter.getRemoteDevice(items[position].deviceAdress)
+                if (device != null) {
+                    when (action) {
+                        DEVICE_ACTION_TYPE.ADD -> {
+                            Log.d(TAG, "click to add : -> ${device.name} : ${device.address} ")
+                            device.createBond()
                         }
-                    }
-                    DEVICE_ACTION_TYPE.NOACTION->{
-                        Log.d(TAG,"click no action")
+                        DEVICE_ACTION_TYPE.REMOVE -> {
+                            Log.d(TAG, "click to remove : -> ${device.name} : ${device.address} ")
+                            try {
+                                device::class.java.getMethod("removeBond").invoke(device)
+                            } catch (e: Exception) {
+                                Log.e(TAG, "Removing bond has been failed. ${e.message}")
+                            }
+                        }
+                        DEVICE_ACTION_TYPE.NOACTION -> {
+                            Log.d(TAG, "click no action")
+                        }
                     }
                 }
             }
